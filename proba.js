f500e947b99f5b8a18ad6effbe6e1027aa22c9e7
Mikhail <m-klikov@yandex.ru>
2022-10-18 11:34:04 +0300

    Готовое задание

--- proba.js
+++ proba.js
@@ -1,13 +1,19 @@
 'use strict'
 
-const productsBts = document.querySelectorAll('.text_add_cart'); //коллекция кнопок
-const item = document.querySelector('.item'); //переменная карточки товара
-const card = document.querySelector('.card_box_nav hidden');//переменная корзины
-const quantity = document.querySelector('.card_box_caunter'); //переменная счетчика корзины
-const fullPrice = document.querySelector('.card_box_nav_total') //переменная итоговой цены
-const itemPrice = document.querySelector('.item_price').textContent; //переменная цены в товаре !!!Передается строкой!!!
+const quantityEl = document.querySelector('.card_box_caunter'); //переменная счетчика корзины card_box_caunter
+const fullPriceEl = document.querySelector('card_box_nav_total') //переменная блока ИТОГО
+const fullPriceValueEl = document.querySelector('.card_box_nav_total_price'); // счетчик итоговой цены переменная ИТОГО куда показывается вся стоимость покупки
+const cardEl = document.querySelector('card_box_nav')//переменная корзины
 
-let price = 0; //переменная цены которая будет пересчитываться и подставляться в ИТОГО
+
+const itemProducts = document.querySelectorAll('.item');// коллекция всех item нужно для присваивания рандомных id
+const productsBts = document.querySelectorAll('.add_link'); //коллекция кнопок
+
+
+const card = {};
+
+
+
 
 /**
  * 
@@ -17,115 +23,103 @@ const randomID = () => {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 };
 
+itemProducts.forEach(el => {
+    el.closest('.item').setAttribute('data-id', randomID());
+});
+
+
+
 /**
  * 
  * @param {принимает строку itemPrice} str 
  * @returns возвращает число из строки без символов для сложения цен
  */
-const transformPriceNum = (priceSrt) => {
-    return parseFloat(priceSrt.slice(1));
-};
-
-/**
- * возвращает цену со знаком $ для подстановки в корзину ИТОГО
- */
-const transformPriceStr = (price) => {
-    return `$${price}`;
-};
-
-/**
- * выводит общую стоимость всех товаров
- */
-const plussFullPrice = (currentPrice) => {
-    currentPrice = transformPriceNum(itemPrice);
-    return Math.round((price += currentPrice) * 100) / 100;
+const transformPriceNum = (currentPrice) => {
+    return parseFloat(currentPrice.slice(1));    //!!!!itWORK
 };
 
 
-/**
- * выводит число в ИТОГО
- */
-const printFullPrice = () => {
-    fullPrice.textContent = `${transformPriceStr(price)}`
-};
 
-const printQuantity = () => {
-    let length = item.querySelector('.card_box_nav').children.length;
-    quantity.textContent = length;
-};
-
-const generateCardProduct = (itemHeader, count, ItemPrice, id) => {
-    return `
-    <div class="card_box_nav hidden">
-         <div class="card_box_nav_title" data-id = ${id}>
-            <div class="card_box_nav_title_name">Товар</div>
-            <div class="card_box_nav_title_name">Количество</div>
-            <div class="card_box_nav_title_name">Цена</div>
-        </div>
-
-        <div class="card_box_nav_item">
-            <ul class="card_box_nav_link">
-                 <li class="card_box_nav_link name">${itemHeader}</li>
-                <li class="card_box_nav_link count">${count}</li>
-                <li class="card_box_nav_link price">${ItemPrice}</li>
-            </ul>
-
-        </div>
-
-        <div class="card_box_nav_total">
-            Итого: <span class="card_box_nav_total_price">${price}</span>
-
-        </div>
-    </div>
-    `;
-};
-
-productsBts.forEach(el => {
-    el.closest('.item').setAttribute('data-id', randomID());
-    el.addEventListener('click', (e) => {
-        let self = e.currentTarget;
-        let parent = self.closest('.item');
-        let id = parent.dataset.id;
-        let itemHeader = parent.querySelector('.item_header').textContent;;
-        let count = parent.querySelector('.item');
-        let pricesStr = parent.querySelector('.item_price').textContent;
-
-        let priceNumber = transformPriceNum(parent.querySelector('.item_price').textContent);
-        console.log(priceNumber);
-
-        // действия:
-        //ссумирует стоимость товара ИТОГО !it Work!
-        plussFullPrice(priceNumber);
-        console.log(plussFullPrice(priceNumber));
-
-        //выводит полную стоимость 
-        printFullPrice();
-
-        //добавляет в корзину товар
-        item.querySelector('.card_box_nav').insertAdjacentHTML('afterbegin', generateCardProduct(itemHeader, count, priceNumber, id))
-
-        //счетает и выводит количество товаров в корзине через счетчик
-        printQuantity()
-
-
-        //добавляет несколько однотипных товаров в корзину
-
-
-
-
-    });
+//добавляем обработчик события "клик" для появления корзины !!!!itWORK
+document.querySelector('.card_img').addEventListener('click', () => {
+    document.querySelector('.card_box_nav').classList.toggle('hidden')
 });
 
+//Добавляем обработчик события по клику на кнопку add to card
+document.querySelector('.item_box').addEventListener('click', event => {
+    if (!event.target.closest('.add_link')) {
+        return;
+    }
+    const itemEl = event.target.closest('.item'); //получаем ближайщего родителя 
+
+    const itemName = event.target.closest('.item').querySelector('.item_header').textContent //получаем наименование товара
+    itemEl.setAttribute('data-name', itemName) // присвоили data-name в item
+
+    const itemPrice = transformPriceNum(event.target.closest('.item').querySelector('.item_price').textContent) //получаем цену товара тип NUMBER
+    itemEl.setAttribute('data-price', itemPrice); // присвоили data-price в item
+
+    const id = itemEl.dataset.id // получаем id родителя установленного в data-id
+    const name = itemEl.dataset.name //получаем name родителя установленного в data-name
+    const price = itemEl.dataset.price //получаем price родителя установленного в data-price
 
 
 
+    //Добавить в корзину товар
+    //addToCard(id, name, price); //нужно прописать функцию добавления товара в корзину
+});
 
+const addToCard = (id, name, price) => {
+    if (!(id in card)) {
+        card[id] = { id: id, name: name, price: price, count: 0 };
+        card[id].count++;
 
+        quantityEl.textContent = getTotalCardCount().toString(); //прописать функцию getTotalBasketCount
 
+        fullPriceValueEl.textContent = getTotalCardPrice().toFixed(2);//прописать функцию
 
+        renderProductInBasket(id); //прописать функцию отрисовки 
+    }
+};
 
+const getTotalCardCount = () => {
+    return Object.values(card).reduce((acc, product) => acc + product.count, 0);
+};
 
+const getTotalCardPrice = () => {
+    return Object
+        .values(card)
+        .reduce((acc, product) => acc + product.price * product.count, 0);
+};
 
+console.log(document.querySelector('.item').dataset.id)
 
+const renderProductInBasket = (itemId) => {
+    const cardRowEl = document
+        .querySelector(`.item`).dataset.id; //card_box_nav_link_title_list
+    if (!cardRowEl) {
+        renderNewProductInBasket(itemId);
+        return;
+    }
+    const product = card[itemId];
+    //cardRowEl.querySelector('.count').textContent = product.count;
 
+    // cardRowEl
+    //     .querySelector('.productTotalRow')
+    //     .textContent = (product.price * product.count).toFixed(2);
 
+};
+
+const renderNewProductInBasket = (id) => {
+    const productRow = `
+    <div class="card_box_nav_link_title_list" data-id = ${id}>
+        <div class="card_box_nav_link name"> ${document.querySelector('.item').dataset.name} </div>
+        <div class="card_box_nav_link count"> ${card[id].count}  </div>
+        <div class="card_box_nav_link price"> ${(card[id].price * card[id].count).toFixed(2)} </div>
+    </div>
+    `
+    document
+        .querySelector('.card_box_nav_link_title_list')
+        .insertAdjacentHTML("beforebegin", productRow);
+}
+
+console.log(document.querySelector('.card_box_nav_link_title_list'))
